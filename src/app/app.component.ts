a3160212b566476ce303b8053a4f3b2880b6f09b
korpozim <korpozim@gmail.com>
2020-01-22 19:29:40 +0300

    add app

---
+++ src/app/app.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import * as olProj from 'ol/proj';
+import XYZ from 'ol/source/XYZ';
+import TileWMS from 'ol/source/TileWMS';
+import BingMaps from 'ol/source/BingMaps';
+
+import 'ol/ol.css';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  public map: Map;
+  public bingMapKey: string = "";
+
+  ngOnInit(): void {
+    this.updateMap();
+  }
+
+  public updateMap() {
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          //source: new OSM(),
+          source: new BingMaps({
+            key: this.bingMapKey,
+            imagerySet: 'Aerial'
+          }),
+          visible: true
+        })
+      ],
+      view: new View({
+        center: olProj.fromLonLat([-7.967367, 53.553648]),
+        zoom: 3
+      })
+    });
+
+    console.log(this.map);
+  }
+
+  public setBingMapKey(text: any) {
+    this.bingMapKey = text;
+
+    this.map.removeLayer(this.map.getLayers(0).getArray()[0]);
+    this.map.addLayer(new TileLayer({
+      source: new BingMaps({
+        key: this.bingMapKey,
+        imagerySet: 'Aerial'
+      }),
+      visible: true
+    }));
+  }
+}
